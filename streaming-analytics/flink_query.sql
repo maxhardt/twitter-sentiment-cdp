c6095598b0e6a96f38fc9d4e69e6df8900346972
Maximilian Engelhardt <mengelhardt@cloudera.com>
2023-01-25 11:49:57 +0100

    qry

--- streaming-analytics/flink_query.sql
+++ streaming-analytics/flink_query.sql
@@ -6,7 +6,7 @@ SELECT
     COUNT(*) as num_tweets,
     CONCAT(CAST(window_start AS STRING), '-', tag, '-', label) as primary_key
 FROM
-    TABLE(TUMBLE(TABLE twitter_auto_data, DESCRIPTOR(eventTimestamp), INTERVAL '1' MINUTES))
+    TABLE(TUMBLE(TABLE twitter_sentiment, DESCRIPTOR(eventTimestamp), INTERVAL '1' MINUTES))
 GROUP BY
     window_start,
     window_end,
